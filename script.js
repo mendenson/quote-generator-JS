557d06e06d070e46caaec42c9bc4901bb145fd85
Mendenson Mota <mendenson@gmail.com>
2023-04-20 17:09:44 -0600

    Project files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get Quotes From API
+async function getQuotes() {
+    const apiURL = './quotes.json';
+    try {
+        const response = await fetch(apiURL);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
