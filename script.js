8776d820fa33022a67beb21dfbe78586510f4267
Mendenson Mota <mendenson@gmail.com>
2023-04-20 21:11:57 -0600

    new updates

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+}
+
 // Get Quotes From API
 async function getQuotes() {
-    const apiURL = './quotes.json';
+    const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
     } catch (error) {
         // Catch Error Here
     }
 }
+
+// On Load
+getQuotes();
