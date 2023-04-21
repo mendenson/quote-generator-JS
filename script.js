d21b39a015ce713622c6d967b3f36ecfe7b54f88
Mendenson Mota <mendenson@gmail.com>
2023-04-21 12:43:16 -0600

    ultimate update

--- script.js
+++ script.js
@@ -7,61 +7,60 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-//Show Loading
+// Loading Spinner Shown
 function loading() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-//Hide Loading
+// Remove Loading Spinner
 function complete() {
-    quoteContainer.hidden = false;
-    loader.hidden = true;
+  quoteContainer.hidden = false;
+  loader.hidden = true;
 }
 
 // Show New Quote
 function newQuote() {
-    loading();
-    // Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    //Check if Author field is blank and replace it with 'Unknown'
-    if (!quote.author) {
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
-    //Check Quote length to determine styling
-    if (quote.text.length > 50) {
-        quoteText.classList.add('long-quote');
-    } else {
-        quoteText.classList.remove('long-quote');
-    }
-    // Set Quote, Hide Loader
-    quoteText.textContent = quote.text;
-    complete();
+  loading();
+  // Pick a random quote from array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
-    loading();
-    const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-    try {
-        const response = await fetch(apiURL);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch (error) {
-        // Catch Error Here
-    }
+  loading();
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
 }
 
-//Tweet Quote
+// Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=
-    ${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl, '_blank');
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, '_blank');
 }
 
-//Event Listeners
+// Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
